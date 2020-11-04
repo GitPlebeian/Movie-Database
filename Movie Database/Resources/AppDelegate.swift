36574aeb4f20dc0c10c62026d797d37291fdfe4b
GitPlebeian <jaxtubbs@gmail.com>
2020-11-04 15:43:11 -0800

    Sets up file system

---
+++ Movie Database/Resources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Movie Database
+//
+//  Created by Jackson Tubbs on 11/4/20.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
